0db779cb423424eafdea717fbd37721f40f5d238
Ľubomír Remák <lubomirr@lubomirr.eu>
2012-02-01 22:49:36 +0100

    Add format-lua.c and fix gettext.texi.

--- gettext-tools/src/x-lua.c
+++ gettext-tools/src/x-lua.c
@@ -1,5 +1,5 @@
 /* xgettext Lua backend.
-   Copyright (C) 2011 Free Software Foundation, Inc.
+   Copyright (C) 2012 Free Software Foundation, Inc.
 
    This file was written by Ľubomír Remák <lubomirr@lubomirr.eu>, 2012.
 
